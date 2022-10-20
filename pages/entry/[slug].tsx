539837219ab4286d5827e60f6d99012a8d0455e8
Juan Gómez <gomezj01@stepstone.com>
2022-10-20 10:06:43 -0600

    Se agregan otras entradas y categorias renderizadas desde el servidor.

--- pages/entry/[slug].tsx
+++ pages/entry/[slug].tsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { getPlant, QueryStatus } from '@api'
+import Link from 'next/link';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { Layout } from '@components/Layout'
 import { RichText } from '@components/RichText'
 import { AuthorCard } from '@components/AuthorCard'
+import { PlantEntryInline } from '@components/PlantCollection';
 import { Grid } from '@ui/Grid'
 import { Typography } from '@ui/Typography'
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { getPlantList } from '../../api/index';
+import { getCategoryList, getPlant, QueryStatus, getPlantList } from '@api'
 
 type PathType = {
   params: {
@@ -31,6 +31,8 @@ export const getStaticPaths = async () => {
 
 type PlantEntryProps = {
   plant: Plant
+  otherEntries: Plant[] | null
+  categories: Category[] | null
 }
 
 export const getStaticProps: GetStaticProps<PlantEntryProps> = async ({ params }) => {
@@ -44,10 +46,14 @@ export const getStaticProps: GetStaticProps<PlantEntryProps> = async ({ params }
 
   try {
     const plant = await getPlant(slug);
+    const otherEntries = await getPlantList({limit: 5})
+    const categories = await getCategoryList({limit: 10});
 
     return {
       props: {
-        plant
+        plant,
+        otherEntries,
+        categories
       }
     }
 
@@ -58,7 +64,7 @@ export const getStaticProps: GetStaticProps<PlantEntryProps> = async ({ params }
   }
 }
 
-export default function PlantEntryPage({ plant }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function PlantEntryPage({ plant, otherEntries, categories }: InferGetStaticPropsType<typeof getStaticProps>) {
   const [status, setStatus] = useState<QueryStatus>('idle');
 
   if(plant === null) {
@@ -73,34 +79,50 @@ export default function PlantEntryPage({ plant }: InferGetStaticPropsType<typeof
 
   return (
     <Layout>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={8} lg={9} component="article">
-          <figure>
-            <img src={ plant.image.url }/>
-          </figure>
-          <div className="px-12 pt-8">
-            <Typography variant="h2">{plant.plantName}</Typography>
-          </div>
-          <div className="p-10">
-            <RichText richText={plant.description} />
-          </div>
+        <Grid container spacing={4} >
+            <Grid item xs={12} md={8} lg={9} component="article">
+                <figure>
+                    <img width={952} src={plant.image.url} alt={plant.image.title} />
+                </figure>
+                <div className="px-12 pt-8">
+                    <Typography variant="h2">{plant.plantName} </Typography>
+                </div>
+                <div className="p-10">
+                    <RichText richText={plant.description} />
+                </div>
+            </Grid>
+            <Grid item xs={12} md={4} lg={3} component="aside">
+                <section>                    
+                    <Typography variant="h5" component="h3" className="mb-4" > 
+                        Recent post
+                    </Typography>
+                </section>
+                {otherEntries?.map((plantEntry) => (
+                    <article className='mb-4' key={plantEntry.id}>
+                        <PlantEntryInline {...plantEntry}/>
+                    </article>
+                ))}
+                <section className='mt-10'>
+                    <Typography variant="h5" component="h3" className='mb-4'>
+                        Categories
+                    </Typography>
+                    <ul className='list'>
+                        {categories?.map((category) => (
+                        <li key={category.id}>
+                            <Link passHref href={`/category/${category.slug}`}>
+                                <Typography variant="h6" component="a">
+                                    {category.title}
+                                </Typography>
+                            </Link>                                 
+                        </li>
+                        ))}
+                    </ul>
+                </section>
+            </Grid>
         </Grid>
-        <Grid item xs={12} md={4} lg={3} component="aside">
-          <section>
-            <Typography variant="h5" component="h3" className="mb-4">
-              Recent Posts
-            </Typography>
-          </section>
-          <section className="mt-10">
-            <Typography variant="h5" component="h3" className="mb-4">
-              Categories
-            </Typography>
-          </section>
-        </Grid>
-      </Grid>
-      <section className="my-4 border-t-2 border-b-2 border-gray-200 pt-12 pb-7">
-        <AuthorCard {...plant.author} />
-      </section>
+        <section className='my-4 border-t-2 border-b-2 border-gray-200 pt-12 pb-7'>
+            <AuthorCard {...plant.author} />
+        </section>
     </Layout>
   )
 }
